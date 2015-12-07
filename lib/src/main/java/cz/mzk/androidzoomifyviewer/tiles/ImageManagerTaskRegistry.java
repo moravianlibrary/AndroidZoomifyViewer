9ec5ef8b3e71b2e132500d1b3c8955e32e719090
rzeh4n <Martin.Rehanek@gmail.com>
2015-12-07 19:04:13 +0100

    More cleanup.

--- lib/src/main/java/cz/mzk/androidzoomifyviewer/tiles/ImageManagerTaskRegistry.java
+++ lib/src/main/java/cz/mzk/androidzoomifyviewer/tiles/ImageManagerTaskRegistry.java
@@ -8,8 +8,8 @@ import cz.mzk.androidzoomifyviewer.Logger;
 import cz.mzk.androidzoomifyviewer.viewer.TiledImageView;
 
 /**
- * This class registers running AsynctTasks in which tiles for single image are downloaded and saved to cache. Purpose of this
- * class is to prevent executing multiple tasks to download same tile. Methods of this class are allways accessed from UI thread,
+ * This class registers running AsyncTasks in which tiles for single image are downloaded and saved to cache. Also AsyncTask to initialize image metadata.
+ * Purpose of this class is to prevent executing multiple task to download same tile. Methods of this class are allways accessed from UI thread,
  * so there's no need for synchronization of internal data (tiles map).
  *
  * @author Martin Řehánek
@@ -20,62 +20,64 @@ public class ImageManagerTaskRegistry {
 
     private static final Logger LOGGER = new Logger(ImageManagerTaskRegistry.class);
 
-    private final Map<TilePositionInPyramid, DownloadAndSaveTileTask> tasks = new HashMap<TilePositionInPyramid, DownloadAndSaveTileTask>();
     private final ImageManager mImgManager;
-    private InitImageManagerTask mInitTask;
+    private final Map<TilePositionInPyramid, DownloadAndSaveTileTask> mTileDownloadTasks = new HashMap<>();
+    private InitImageManagerTask mInitMetadataTask;
 
     public ImageManagerTaskRegistry(ImageManager imgManager) {
         this.mImgManager = imgManager;
     }
 
     public void enqueueTileDownloadTask(final TilePositionInPyramid tilePositionInPyramid, String mZoomifyBaseUrl, TiledImageView.TileDownloadErrorListener errorListener, TiledImageView.TileDownloadSuccessListener successListener) {
-        if (tasks.size() < MAX_TASKS_IN_POOL) {
-            boolean registered = tasks.containsKey(tilePositionInPyramid);
-            if (!registered) {
+        if (mTileDownloadTasks.size() < MAX_TASKS_IN_POOL) {
+            if (!mTileDownloadTasks.containsKey(tilePositionInPyramid)) {
+                LOGGER.d("enqueuing tile-download task for " + tilePositionInPyramid + ": (total " + mTileDownloadTasks.size() + ")");
                 // TODO: 7.12.15 proc se posila zoomfyBaseUrl?
                 DownloadAndSaveTileTask task = new DownloadAndSaveTileTask(mImgManager, mZoomifyBaseUrl, tilePositionInPyramid, errorListener, successListener, new TaskFinishedListener() {
 
                     @Override
                     public void onTaskFinished() {
-                        tasks.remove(tilePositionInPyramid);
+                        mTileDownloadTasks.remove(tilePositionInPyramid);
                     }
                 });
-                tasks.put(tilePositionInPyramid, task);
-                LOGGER.v("enqueued tile-download task for " + tilePositionInPyramid + ": (total " + tasks.size() + ")");
+                mTileDownloadTasks.put(tilePositionInPyramid, task);
                 task.executeConcurrentIfPossible();
             } else {
-                LOGGER.v("ignoring tile-download task for " + tilePositionInPyramid + ", already in queue");
+                LOGGER.d("ignoring tile-download task for " + tilePositionInPyramid + ", already in queue");
             }
         } else {
-            LOGGER.v("ignoring tile-download task for " + tilePositionInPyramid + ", queue full (" + tasks.size() + " tasks)");
+            LOGGER.d("ignoring tile-download task for " + tilePositionInPyramid + ", queue full (" + mTileDownloadTasks.size() + " mTileDownloadTasks)");
         }
     }
 
     public void enqueueMetadataInitializationTask(TiledImageView.MetadataInitializationHandler handler, TiledImageView.MetadataInitializationSuccessListener successListener) {
-        mInitTask = new InitImageManagerTask(mImgManager, handler, successListener, new TaskFinishedListener() {
+        if (mInitMetadataTask == null) {
+            LOGGER.d("enqueuing metadata-initialization task");
+            mInitMetadataTask = new InitImageManagerTask(mImgManager, handler, successListener, new TaskFinishedListener() {
 
-            @Override
-            public void onTaskFinished() {
-                mInitTask = null;
-            }
-        });
-        LOGGER.v("enqueued metadata-initialization task");
-        mInitTask.executeConcurrentIfPossible();
-
+                @Override
+                public void onTaskFinished() {
+                    mInitMetadataTask = null;
+                }
+            });
+            mInitMetadataTask.executeConcurrentIfPossible();
+        } else {
+            LOGGER.d("ignoring metadata-initialization task - already in queue");
+        }
     }
 
     public void cancelAllTasks() {
-        if (mInitTask != null) {
-            mInitTask.cancel(false);
-            mInitTask = null;
+        if (mInitMetadataTask != null) {
+            mInitMetadataTask.cancel(false);
+            mInitMetadataTask = null;
         }
-        for (DownloadAndSaveTileTask task : tasks.values()) {
+        for (DownloadAndSaveTileTask task : mTileDownloadTasks.values()) {
             task.cancel(false);
         }
     }
 
     public boolean cancel(TilePositionInPyramid id) {
-        DownloadAndSaveTileTask task = tasks.get(id);
+        DownloadAndSaveTileTask task = mTileDownloadTasks.get(id);
         if (task != null) {
             task.cancel(false);
             return true;
@@ -85,7 +87,7 @@ public class ImageManagerTaskRegistry {
     }
 
     public Set<TilePositionInPyramid> getAllTileDownloadTaskIds() {
-        return tasks.keySet();
+        return mTileDownloadTasks.keySet();
     }
 
 
