f4bf65a135b50d72ed16d585c1c61cb680cbb802
rzeh4n <Martin.Rehanek@gmail.com>
2016-05-21 22:02:40 +0200

    demo app: intro with first page (unfinished)

---
+++ app/src/main/java/cz/mzk/tiledimageview/demonstration/intro/IntroActivity.java
@@ -0,0 +1,79 @@
+package cz.mzk.tiledimageview.demonstration.intro;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+
+import cz.mzk.tiledimageview.demonstration.R;
+
+/**
+ * Created by Martin Řehánek on 20.5.16.
+ */
+public class IntroActivity extends AppIntro {
+
+    // Please DO NOT override onCreate. Use init.
+    @Override
+    public void init(Bundle savedInstanceState) {
+
+        // Add your slide's fragments here.
+        // AppIntro will automatically generate the dots indicator and buttons.
+        /*addSlide(first_fragment);
+        addSlide(second_fragment);
+        addSlide(third_fragment);
+        addSlide(fourth_fragment);*/
+
+//        FragmentTransaction trans = getSupportFragmentManager().beginTransaction();
+        IntroTiles fragment = new IntroTiles();
+//        trans.add(fragment, "intro1");
+//        trans.commit();
+
+        addSlide(fragment);
+
+
+        // Instead of fragments, you can also use our default slide
+        // Just set a title, description, background and image. AppIntro will do the rest.
+        //addSlide(AppIntroFragment.newInstance(title, description, image, background_colour));
+        addSlide(AppIntroFragment.newInstance("second", "red", R.mipmap.ic_launcher, R.color.red));
+        addSlide(AppIntroFragment.newInstance("third", "green", R.mipmap.ic_launcher, R.color.green));
+        addSlide(AppIntroFragment.newInstance("fourth", "blue", R.mipmap.ic_launcher, R.color.blue));
+
+
+        // OPTIONAL METHODS
+        // Override bar/separator color.
+        setBarColor(Color.parseColor("#3F51B5"));
+        setSeparatorColor(Color.parseColor("#2196F3"));
+
+        // Hide Skip/Done button.
+        showSkipButton(false);
+        setProgressButtonEnabled(false);
+
+        // Turn vibration on and set intensity.
+        // NOTE: you will probably need to ask VIBRATE permisssion in Manifest.
+        setVibrate(true);
+        setVibrateIntensity(30);
+    }
+
+    @Override
+    public void onSkipPressed() {
+        // Do something when users tap on Skip button.
+    }
+
+    @Override
+    public void onDonePressed() {
+        // Do something when users tap on Done button.
+    }
+
+    @Override
+    public void onSlideChanged() {
+        // Do something when the slide changes.
+    }
+
+    @Override
+    public void onNextPressed() {
+        // Do something when users tap on Next button.
+    }
+
+
+}
