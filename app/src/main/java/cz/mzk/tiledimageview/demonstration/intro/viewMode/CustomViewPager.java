369f8332a5590536f9f5c5a0e99e1745bb184f1f
rzeh4n <Martin.Rehanek@gmail.com>
2016-05-23 16:09:41 +0200

    demo app: viewMode intro example - disabled swapping in ViewPager

---
+++ app/src/main/java/cz/mzk/tiledimageview/demonstration/intro/viewMode/CustomViewPager.java
@@ -0,0 +1,37 @@
+package cz.mzk.tiledimageview.demonstration.intro.viewMode;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * ViewPager with disabled swipe gesture, which would here be in conflict with TiledImageView gestures.
+ * Created by Martin Řehánek on 23.5.16.
+ */
+public class CustomViewPager extends ViewPager {
+
+    private boolean isPagingEnabled = false;
+
+    public CustomViewPager(Context context) {
+        super(context);
+    }
+
+    public CustomViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return this.isPagingEnabled && super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        return this.isPagingEnabled && super.onInterceptTouchEvent(event);
+    }
+
+    public void setPagingEnabled(boolean b) {
+        this.isPagingEnabled = b;
+    }
+}
