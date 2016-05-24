4af67c8995d3572cb722a6ddfc474a203c57ad63
rzeh4n <Martin.Rehanek@gmail.com>
2016-05-24 19:44:38 +0200

    demo app: thumbnail for caching

--- app/src/main/java/cz/mzk/tiledimageview/demonstration/intro/IntroActivity.java
+++ app/src/main/java/cz/mzk/tiledimageview/demonstration/intro/IntroActivity.java
@@ -19,12 +19,11 @@ public class IntroActivity extends AppIntro {
 
         // Add your slide's fragments here.
         // AppIntro will automatically generate the dots indicator and buttons.
-
-        addSlide(new IntroCachingFragment());//5
         addSlide(new IntroTilesFragment());//1
         addSlide(new IntroGesturesFragment());//2
         addSlide(new IntroViewmodeFragment());//3
         addSlide(new IntroRectanglesFragment());//4
+        addSlide(new IntroCachingFragment());//5
 
 
         // OPTIONAL METHODS
